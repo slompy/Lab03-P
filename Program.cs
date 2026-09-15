26319557d267c919fe5e3b3ac061ff22c0b725c4
slompy <krakenov.slava>
2026-09-15 14:23:28 +0300

    Lab03: compound assinment operators

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Банковский счёт");
+
+double balance = 1000;
+Console.WriteLine($"Начальный баланс: {balance}");
+
+balance += 500; // Пополнение баланс
+Console.WriteLine($"После пополнения на 500: {balance}");
+
+balance -= 200; // Покупка
+Console.WriteLine($"После покупки на 200: {balance}");
+
+balance *= 1.05; // Начисление 5% 
+Console.WriteLine($"После начисления 5%: {balance}");
+
+balance /= 2; // Разделение счёта пополам с партнёром
+Console.WriteLine($"После деления пополам: {balance}");
+
+
